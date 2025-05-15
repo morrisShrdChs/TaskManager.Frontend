02839f975c3d3b1cf87e7abc089d9114c0501e9a
amilliondegrees <amilliondegrees@DESKTOP-T80C932>
2025-05-15 23:19:30 +0700

    Изменен стиль у страницы логина, применены стили к странице создания задачи

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,7 +4,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginData = { userName: '', password: '' };
